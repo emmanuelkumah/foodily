a39831b114497b0e606a5188743722a58fe2f08f
EmmanuelKumah <mailkumah@gmail.com>
2022-03-24 15:12:07 +0000

    Styling of navbar

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import classes from "./Cart.module.css";
 
 function Cart() {
   return (
     <div>
       <IconContext.Provider value={{ color: "white" }}>
         <FaShoppingCart />
-        <span>5</span>
+        <span className={classes.cartNotf}>5</span>
       </IconContext.Provider>
     </div>
   );
