e0166c37e3e75c5364900c44ac234ebde2516ae1
EmmanuelKumah <mailkumah@gmail.com>
2022-03-30 13:48:09 +0000

    Complete menuCards

--- src/Components/MenuDishes/MenuDish.js
+++ src/Components/MenuDishes/MenuDish.js
@@ -3,25 +3,25 @@ import dishes from "./dishData";
 import classes from "./MenuDish.module.css";
 
 function MenuDish() {
-  console.log(dishes);
-  const menuItem = dishes.map(function (dish) {
+  const menuCardItem = dishes.map(function (dish) {
     return (
-      <div className={classes.menuCardItem}>
-        <img src={dish.img} alt="dishes" className={classes.menuCardImg} />
-        <h3>{dish.name}</h3>
-        <p className={classes.menuAmt}>{`$${dish.amt}`}</p>
-        <p className={classes.menuCardDetls}>{`${dish.desc.slice(
-          0,
-          120
-        )}...`}</p>
-        <button className={classes.btnOrder}>Add To Cart</button>
+      <div className={classes["menuCard--item"]}>
+        <div className={classes["menuCard--cover"]}>
+          <img src={dish.img} alt="menuCover" />
+        </div>
+        <div className={classes["menuCard--content"]}>
+          <h3>{dish.name}</h3>
+          <p>{`${dish.desc.slice(0, 120)} ...`}</p>
+          <p className={classes["menuCard--amt"]}>{`$${dish.amt}`}</p>
+          <button className={classes["menuCard-btn"]}>Add to Card</button>
+        </div>
       </div>
     );
   });
   return (
     <section className={classes.mainMenuContainer}>
-      <h1>Best Selling Menus</h1>
-      <div className={classes.menuCards}>{menuItem}</div>
+      <h3 className={classes.menuHeading}>Yummy Meals Everyday</h3>
+      <div className={classes.menuCards}>{menuCardItem}</div>
     </section>
   );
 }
--- src/Components/MenuDishes/dishData.js
+++ src/Components/MenuDishes/dishData.js
@@ -5,7 +5,7 @@ const dishes = [
     desc:
       "Give mince a makeover with this American-inspired favourite. Known as a ‘loose meat’ sandwich, this saucy, savoury filling can be adapted to your tastes",
     amt: 10,
-    img: "https://source.unsplash.com/PxJ9zkM2wdA",
+    img: "https://source.unsplash.com/E94j3rMcxlw/600x800",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const dishes = [
     desc:
       "Pack your pitta with peppers, chicken and tzatziki for a low-calorie lunch option. Plate up your chicken sandwich in 15 minutes and enjoy 3 of your 5-a-day",
     amt: 15,
-    img: "https://source.unsplash.com/CbNAuxSZTFo",
+    img: "https://source.unsplash.com/CbNAuxSZTFo/600x800",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const dishes = [
     desc:
       "The fillings for your sandwiches are baked into the bread here, making this an easy picnic option. To make it veggie, look for a vegetarian pesto",
     amt: 43,
-    img: "https://source.unsplash.com/kcA-c3f_3FE",
+    img: "https://source.unsplash.com/kcA-c3f_3FE/600x800",
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ const dishes = [
     desc:
       "Take the cheese toastie to another level with this four-cheese grilled sandwich – a tasty mix of ricotta, parmesan, mozzarella and gorgonzola on white bread",
     amt: 8,
-    img: "https://source.unsplash.com/_LGrizFCwqM",
+    img: "https://source.unsplash.com/_LGrizFCwqM/600x800",
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const dishes = [
     desc:
       "Jazz up your bangers with a sweet honey mustard and soy glaze and serve in a French stick sandwich with salad",
     amt: 21,
-    img: "https://source.unsplash.com/m_N0Q_lwxZ0",
+    img: "https://source.unsplash.com/m_N0Q_lwxZ0/600x800",
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ const dishes = [
     desc:
       "Enjoy this healthy chicken traybake with a speedy spinach side that's packed with iron. It's also low in fat and contains four of your 5-a-day",
     amt: 45,
-    img: "https://source.unsplash.com/HQ22vVXhWcc",
+    img: "https://source.unsplash.com/HQ22vVXhWcc/600x800",
   },
   {
     id: 7,
@@ -53,7 +53,7 @@ const dishes = [
     desc:
       "Throw together this wholesome chicken traybake for the family, gently spiced using a little curry paste, with just 15 mins prep. Mango chutney adds a fruity tang",
     amt: "78",
-    img: "https://source.unsplash.com/ND3edEmzcdQ",
+    img: "https://source.unsplash.com/ND3edEmzcdQ/600x800",
   },
   {
     id: 8,
@@ -61,7 +61,7 @@ const dishes = [
     desc:
       "This classic bourbon cocktail takes a festive turn with a splash of orange juice and edible gold glitter - perfect for Christmas parties",
     amt: 16,
-    img: "https://source.unsplash.com/8OpyEpJVgiQ",
+    img: "https://source.unsplash.com/8OpyEpJVgiQ/600x800",
   },
 ];
 
