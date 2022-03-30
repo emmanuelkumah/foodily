02464907a73fadd7ef2fa5be768f4461296b8c82
EmmanuelKumah <mailkumah@gmail.com>
2022-03-30 21:19:21 +0000

    Added meals locator to Meals

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Components/Hero/Hero";
 import MenuCat from "./Components/MenuCat/MenuCat";
 import MenuDish from "./Components/MenuDishes/MenuDish";
+import MenuLoc from "./Components/MenuLocator/MenuLoc";
 import NavBar from "./Components/NavBar/NavBar";
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         <MenuCat />
         <MenuDish />
       </main>
+      <footer>
+        <MenuLoc />
+      </footer>
     </>
   );
 }
---
+++ src/Components/MenuLocator/MenuLoc.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./menuLoc.module.css";
+
+function menuLoc() {
+  return (
+    <section className={classes["subFooter--wraper"]}>
+      <div className={classes["col"]}>
+        <h4 className={classes.footerHeading}>
+          Discover restuarants that deliver near you
+        </h4>
+      </div>
+      <div className={classes["col"]}>
+        <input type="text" className={classes.restLoc} />
+        <button className={classes["btn--go"]}>Go</button>
+      </div>
+    </section>
+  );
+}
+
+export default menuLoc;
