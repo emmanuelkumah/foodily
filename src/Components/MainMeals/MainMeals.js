8ecd627ace3c9179a98f522e7d780e7f3f423cc9
Emmanuel <mailkumah@gmail.com>
2022-04-28 12:47:56 +0000

    Save MainMeal.js file

--- src/Components/MainMeals/MainMeals.js
+++ src/Components/MainMeals/MainMeals.js
@@ -1,36 +1,6 @@
 import React, { useContext } from "react";
 import meals from "./availableMealsData";
 import classes from "./MainMeals.module.css";
-<<<<<<< HEAD
-import { BsCartCheck } from "react-icons/bs";
-import CartContext from "../../store/cartContext";
-
-function MainMeals() {
-  const cartContext = useContext(CartContext);
-  console.log("this context is", cartContext);
-
-  const mealCardItem = meals.map(function (meal) {
-    return (
-      <div
-        key={meal.id}
-        className={`${classes["mealCard--item"]} ${classes["hvr-grow"]}`}
-      >
-        <div className={classes["mealCard--cover"]}>
-          <img src={meal.img} alt="mealCover" />
-        </div>
-        <div className={classes["mealCard--content"]}>
-          <h3>{meal.name}</h3>
-          <p>{`${meal.desc.slice(0, 120)} ...`}</p>
-          <p className={classes["mealCard--amt"]}>{`$${meal.amt}`}</p>
-          <button
-            className={classes["mealCard-btn"]}
-            onClick={() => cartContext.addItem(meal)}
-          >
-            <BsCartCheck /> Add to Cart
-          </button>
-        </div>
-      </div>
-=======
 import SingleMeal from "./SingleMeal";
 
 function MainMeals() {
@@ -43,7 +13,6 @@ function MainMeals() {
         desc={meal.desc}
         amt={meal.amt}
       />
->>>>>>> SingleMeal
     );
   });
   return (
